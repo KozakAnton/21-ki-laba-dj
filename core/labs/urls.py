6f0c90e952f64ab192622386961ab293c28f4680
Kozak Anton <kozakanton18@gmail.com>
2026-04-16 21:04:33 +0300

    Lab 3: Create app with templates, render and context

--- core/labs/urls.py
+++ core/labs/urls.py
@@ -4,4 +4,5 @@ from labs import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('orders/', views.orders_list, name='orders'), # Новий шлях
 ]
--- core/labs/views.py
+++ core/labs/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
+from .models import Product, Category, Order  # Обов'язково додай Order сюди!
 
 def index(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
     context = {
-        'title': 'Головна сторінка проекту',
-        'content': 'Вітаємо! Тут ви знайдете посилання на всі лабораторні роботи.'
+        'title': 'Streethouse - Головна',
+        'categories': categories,
+        'products': products,
     }
     return render(request, 'labs/index.html', context)
 
 def about(request):
+    categories = Category.objects.all()
     context = {
-        'title': 'Про Лабу 1',
-        'content': 'Ця сторінка демонструє використання контексту в Django.'
+        'title': 'Про проект STREETHOUSE',
+        'categories': categories,
     }
     return render(request, 'labs/about.html', context)
+
+def orders_list(request):
+    orders = Order.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'title': 'Мої замовлення',
+        'orders': orders,
+        'categories': categories,
+    }
+    return render(request, 'labs/orders.html', context)
